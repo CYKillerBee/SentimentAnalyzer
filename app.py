a3f5d6130c0d580d576ae2bf7f1cf7c9e443f600
Yih Kit <cheowyk-wm18@student.tarc.edu.my>
2021-11-08 00:33:30 +0800

    trial completion of sentiment analyzer app

--- app.py
+++ app.py
@@ -1,36 +1,55 @@
-import nltk
-import re
-import string
 import pandas as pd
-from nltk.corpus import stopwords
-from sklearn.feature_extraction.text import TfidfVectorizer
 from flask import Flask, render_template, request
+from sentiment_process import clean_text
+import pickle
+import sqlite3
 
 app = Flask(__name__)
 
+conn = sqlite3.connect('database.db')
+print("The database is accessed successfully")
+
+conn.execute("CREATE TABLE if not exists sentiment_db (id INTEGER PRIMARY KEY AUTOINCREMENT,text TEXT,sentiment TEXT)")
+print("Table created successfully")
+conn.close()
+
+model = pickle.load(open('model.pkl', 'rb'))
+vectorized = pickle.load(open('vectorized.pkl', 'rb'))
+
 
 @app.route('/')
+@app.route('/home')
 def homepage():
     return render_template('Home.html', title="Home")
 
 
-@app.route('/', methods=['POST'])
+@app.route('/result', methods=['POST'])
 def my_form_post():
-    if request.method == 'POST':
-        text = request.form['text']
-        nltk.download('vader_lexicon')
-        from nltk.sentiment.vader import SentimentIntensityAnalyzer
-        sid = SentimentIntensityAnalyzer()
-        score = (sid.polarity_scores(str(text)))['compound']
-
-        if score > 0:
-            label = 'Positive'
-        elif score == 0:
-            label = 'Neutral'
-        else:
-            label = 'Negative'
-
-    return render_template('Home.html', result=label)
+    global conn, label
+    text = request.form['text']
+    print('Text: ', text)
+    cleaned_text = clean_text.text_cleaner(text)
+    test_bag = vectorized.transform([cleaned_text]).toarray()
+    prediction = model.predict(test_bag)
+    print('Prediction: ', prediction)
+    if prediction == ['Positive']:
+        label = 'Positive'
+    elif prediction == ['Negative']:
+        label = 'Negative'
+    else:
+        label = 'Neutral'
+    try:
+        conn = sqlite3.connect('database.db')
+        cur = conn.cursor()
+        cur.execute("INSERT INTO sentiment_db(text,sentiment) VALUES (?,?)", (text, label))
+        conn.commit()
+        feedback_id = cur.lastrowid
+        return render_template('result.html', text=text, result=label, feedback_id=feedback_id)
+    except:
+        print("Error detected")
+        conn.rollback()
+    finally:
+        conn.close()
 
 
 @app.route('/analysis')
@@ -38,6 +57,18 @@ def view_analysis():
     data = pd.read_csv('final-sentiment-data.csv', header=0)
     myData = list(data.values)
     return render_template('Analysis.html', title="Analysis", myData=myData)
+
+
+@app.route('/data')
+def view_data():
+    conn = sqlite3.connect("database.db")
+    conn.row_factory = sqlite3.Row
+
+    cur = conn.cursor()
+    cur.execute("select * from sentiment_db")
+
+    rows = cur.fetchall()
+    return render_template("data.html", title="Data", rows=rows)
 
 
 if __name__ == '__main__':
---
+++ sentiment_process/clean_text.py
@@ -0,0 +1,25 @@
+import re
+import string
+
+
+def text_cleaner(textData):
+    # remove tab, new line, ans back slice
+    textData = textData.replace('\\t', " ").replace('\\n', " ").replace('\\u', " ").replace('\\', "")
+    # remove non ASCII (emoticon, chinese word, .etc)
+    textData = textData.encode('ascii', 'replace').decode('ascii')
+    # remove mention, link, hashtag
+    textData = ' '.join(re.sub("([@#][A-Za-z0-9]+)|(\w+:\/\/\S+)", " ", textData).split())
+    # remove incomplete URL
+    textData = textData.replace("http://", " ").replace("https://", " ")
+    # remove non-English characters
+    textData = re.sub("([^\x00-\x7F])+", " ", textData)
+    # remove number
+    textData = re.sub(r"\d+", "", textData)
+    # remove punctuation
+    textData = textData.translate(str.maketrans("", "", string.punctuation))
+    # remove whitespace leading & trailing
+    textData = textData.strip()
+    # remove multiple whitespace into single whitespace
+    textData = re.sub('\s+', ' ', textData)
+
+    return textData
